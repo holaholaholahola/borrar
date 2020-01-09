146725295c96ffb7788ad65ac70e090c2dedea1f
harveycamilo99-mendoza <52213297+harveycamilo99-mendoza@users.noreply.github.com>
2020-01-09 09:42:51 -0500

    fdgf
    fdgdf

--- ddd.cpp
+++ ddd.cpp
@@ -6,6 +6,7 @@ void *hilo(void *valor){
     char *texto = (char *) valor;
     struct timespec tiempo ={1,0};
     while(1){
+        
            printf("%s \n",texto);
           pthread_delay_np(&tiempo);
             }
@@ -14,8 +15,8 @@ void *hilo(void *valor){
 int main(void){
 pthread_t hola1;
 pthread_t hola2;
-pthread_create(&hola1, NULL, &hilo, "Proceso");
-pthread_create(&hola2, NULL, &hilo, "Proceso");
+pthread_create(&hola1, NULL, &hilo, (void *)"Proceso 1");
+pthread_create(&hola2, NULL, &hilo, (void *)"Proceso 2");
 pthread_join(hola1, NULL);
 pthread_join(hola2, NULL);
 return 0;
